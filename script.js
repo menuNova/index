235801a885ccf5433f2131d996c3d633a405eb49
thisSasha <thisDevSasha@gmail.com>
2025-02-22 23:07:30 +0300

    v 2.0

--- script.js
+++ script.js
@@ -8,3 +8,38 @@ document.querySelectorAll('button').forEach((el) => {
         };
     };
 });
+
+
+const data = [
+    ['Несколько языков', 1, 1, 1],
+    ['Изменение меню в таблице', 1, 1, 1],
+    ['Отправка заказов в телеграм', 1, 1, 1],
+    ['Улучшенный дизайн', 0, 1, 1],
+    ['Скидки на продукты', 0, 1, 1],
+    ['Акции. Пример: Обеденное меню', 0, 0, 1],
+    ['Статистика заказов', 0, 0, 1],
+];
+
+const cardLists = document.querySelectorAll('.card__list');
+
+data.forEach(row => {
+    const serviceName = row[0];
+    cardLists.forEach((list, index) => {
+        const flag = row[index + 1];
+        const li = document.createElement('li');
+        li.classList.add('card__function');
+        const icon = document.createElement('i');
+
+        icon.classList.add('fas');
+        icon.classList.add(flag === 1 ? 'fa-check' : 'fa-xmark');
+
+        const p = document.createElement('p');
+        p.textContent = serviceName;
+
+        li.appendChild(icon);
+        li.appendChild(p);
+
+        list.appendChild(li);
+    });
+});
+
